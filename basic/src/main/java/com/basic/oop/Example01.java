b80dd6bda39c0551b1b7d322acb029f0e5bd9d65
wangyufei <wangyf_17@163.com>
2018-04-21 10:57:47 +0800

    提交example01

--- basic/src/main/java/com/basic/oop/Example01.java
+++ basic/src/main/java/com/basic/oop/Example01.java
@@ -6,6 +6,13 @@ import java.util.Properties;
 public class Example01 {
 	
 	public static void main(String[] args) {
+		
+		// 获取
+		Runtime rt = Runtime.getRuntime();
+		System.out.println("处理器的个数:" + rt.availableProcessors() + "个");
+		System.out.println("空闲内存数量:" + rt.freeMemory() / 1024 / 1024 + "M");
+		System.out.println("最大可用内存数量:" + rt.maxMemory() / 1024 / 1024 + "M");
+		
 		// 获取当前系统属性
 		Properties properties = System.getProperties();
 		// 获得所有系统属性的key，返回Enumeration对象
