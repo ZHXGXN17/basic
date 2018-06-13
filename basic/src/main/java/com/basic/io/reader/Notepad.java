9de18541a4a8698e5d99ad472429344b9e424ed9
wangyufei <wangyf_17@163.com>
2018-06-13 21:16:34 +0800

    记事本demo提交

--- basic/src/main/java/com/basic/io/reader/Notepad.java
+++ basic/src/main/java/com/basic/io/reader/Notepad.java
@@ -16,6 +16,9 @@
  */
 package com.basic.io.reader;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -31,8 +34,33 @@ public class Notepad {
 	/** 文件内容. */
 	private static String message = "";
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("--1:新建文件 2:打开文件  3:修改文件  4:保存 5:退出--");
+		while(true) {
+			System.out.println("请输入操作指令!");
+			int command = sc.nextInt();
+			switch(command) {
+			case 1:
+				createFile();
+				break;
+			case 2:
+				openFile();
+				break;
+			case 3:
+				editFile();
+				break;
+			case 4:
+				saveFile();
+				break;
+			case 5:
+				exit();
+				break;
+			default:
+				System.out.println("您输入的指令有误,请重新输入!");
+				break;
+			}
+		}
 	}
 	
 	/**
@@ -60,12 +88,107 @@ public class Notepad {
 		message = stb.toString();
 	}
 	
+	
+	/**
+	 * 打开文件
+	 * @throws Exception
+	 */
 	private static void openFile() throws Exception{
 		// 打开文件时，暂存内容清空
 		message = "";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("请输入打开文件的位置:");
+		// 获取打开文件路径
 		filePath = sc.nextLine();
+		// 控制只能输入txt格式的文件路径
+		if(filePath != null && !filePath.endsWith(".txt")) {
+			System.out.println("请选择文本文件");
+			return;
+		}
+		// 实例化一个FileReader对象
+		FileReader fr = new FileReader(filePath);
+		char[] ch = new char[1024];
+		int len = 0;
+		StringBuffer sb = new StringBuffer();
+		// 循环读取，一次读取一个字符数组
+		while((len = fr.read(ch)) != -1) {
+			sb.append(ch);
+		}
+		
+		// 将打开文件内容暂存
+		System.out.println("打开文件内容:" + "\r\n" + message);
+		fr.close();
 	}
-
+	
+	
+	/**
+	 * 修改文件内容，通过字符串替换的形式
+	 */
+	private static void editFile() {
+		if(message == "" && filePath == null) {
+			System.out.println("请先新建文件或者打开文件");
+			return;
+		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入要修改的内容(以\"修改的目标文件:修改之后的文字\"格式)," + "停止修改请输入\"stop\":");
+		
+		String inputMessage = "";
+		// 当输入stop时，停止修改
+		while(!inputMessage.equals("stop")) {
+			inputMessage = sc.nextLine();
+			if(inputMessage != null && inputMessage.length() > 0) {
+				// 将输入的文字根据“:” 拆分成数组
+				String[] editMessage = inputMessage.split(":");
+				if(editMessage != null && editMessage.length > 1) {
+					// 根据输入的信息将文件中内容进行替换
+					message = message.replace(editMessage[0], editMessage[1]);
+				}
+			}
+		}
+		System.out.println("修改后的内容:" + "\r\n" + message);
+	}
+	
+	
+	/**
+	 * 保存 新建文件存在用户输入的路径 打开的文件将原文件覆盖
+	 * @throws IOException
+	 */
+	private static void saveFile() throws IOException{
+		Scanner sc = new Scanner(System.in);
+		FileWriter out = null;
+		// 文件是由"打开"载入的
+		if(filePath != null) {
+			// 将源文件覆盖
+			out = new FileWriter(filePath);
+		}else {
+			System.out.println("请输入文件保存的绝对路径:");
+			String path = sc.next();
+			filePath = path;
+			// 将输入路径中大写字符替换成小写字母后判断是不是文本格式
+			if(!filePath.toLowerCase().endsWith(".txt")) {
+				filePath += ".txt";
+			}
+			// 构造输出流
+			out = new FileWriter(filePath);
+		}
+		
+		// 写入暂存的内容
+		out.write(message);
+		// 关闭输出流
+		out.close();
+		// 修改文件前将现写入内容置空
+		message = "";
+		// 将文件路径置为null
+		filePath = "";
+	}
+	
+	
+	/**
+	 * 退出
+	 */
+	private static void exit() {
+		System.out.println("您已退出系统，谢谢您的使用!");
+		System.exit(0);
+	}
+	
 }
